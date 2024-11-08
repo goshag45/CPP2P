ee583d4d4a56a3eaca588b3b0f967d734b80a2f1
goshag45 <goshagugl44@gmail.com>
2024-11-08 20:13:01 +1100

    brief but important step (cant include empty folders in gitignore)

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,15 @@ int main() {
     A C++ compiler - im using https://www.msys2.org/
 
     BUILD PROCESS:
-    1. 'cd build' in terminal
+    1. make a folder called 'build' in root
+    2. 'cd build' in terminal
     # make sure build folder is empty
 
-    2. cmake -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF ../
+    3. cmake -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF ../
     # if there are any issues empty the build folder
     # if you have issues with this its probably compiler diff or not added to path 
 
-    3. cmake --build .\
-    4. voila, you have .exe file in build/src
+    4. cmake --build .\
+    5. voila, you have .exe file in build/src
 
 */
